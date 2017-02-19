03a4f24cbfc0ba467ea13b1b6009211582302147
Roman Liutikov <roman01la@romanliutikov.com>
2017-02-19 12:12:24 +0200

    update devcards designs

--- cheetah/src/designs/components.cljs
+++ cheetah/src/designs/components.cljs
@@ -6,8 +6,16 @@
   "<div class=\"screen chat\">
 
   <header class=\"app-header\">
-    <div class=\"screen-title\">Profile</div>
-    <button class=\"menu-btn\">☰</button>
+    <div class=\"left\">
+      <button class=\"back btn\"><</button>
+      <div class=\"screen-title\">
+        <span>ClojureScript</span>
+        <div class=\"room-users-count\">Online: 3 / 5</div>
+      </div>
+    </div>
+    <div class=\"right\">
+      <button class=\"menu-btn\">☰</button>
+    </div>
   </header>
 
   <main>
@@ -15,20 +23,21 @@
     <ul class=\"chat-list\">
 
       <li class=\"chat-msg\">
-        <img src=\"http://i.imgur.com/1rm5Qq6.png\" class=\"avatar\">
+        <img src=\"https://2013.webrebels.org/gfx/speaker_david.jpg\" class=\"avatar\">
         <div class=\"bubble\">
-          <div class=\"bubble-uname\">Alexey Veligura</div>
-          <div class=\"body\">Так как многие не попадают в том числе я ). С вас хорошая трансляция или запись ))</div>
+          <div class=\"bubble-uname\">David Nolen</div>
+          <div class=\"body\">The amount of innovation and invention in the ClojureScript community is nothing short of stunning.</div>
         </div>
       </li>
 
       <li class=\"chat-msg owner\">
-        <img src=\"http://i.imgur.com/1rm5Qq6.png\" class=\"avatar\">
+        <img src=\"https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/3/000/024/157/2fd1a24.jpg\" class=\"avatar\">
         <div class=\"bubble\">
-          <div class=\"bubble-uname_owner\">Alexey Veligura</div>
-          <div class=\"body\">Так как многие не попадают в том числе я ). С вас хорошая трансляция или запись ))</div>
+          <div class=\"bubble-uname_owner\">Rich Hickey</div>
+          <div class=\"body\">Cool!</div>
         </div>
       </li>
+
     </ul>
 
   </main>
@@ -36,6 +45,7 @@
   <footer class=\"app-footer\">
     <form class=\"message\">
       <input type=\"text\" class=\"message\">
+      <button class=\"btn send-btn\">Send</button>
     </form>
   </footer>
 
@@ -46,8 +56,16 @@
 
 (defui Header
   "<header class=\"app-header\">
-  <div class=\"screen-title\">Profile</div>
-  <button class=\"menu-btn\">☰</button>
+  <div class=\"left\">
+    <button class=\"back btn\"><</button>
+    <div class=\"screen-title\">
+      <span>ClojureScript</span>
+      <div class=\"room-users-count\">Online: 3 / 5</div>
+    </div>
+  </div>
+  <div class=\"right\">
+    <button class=\"menu-btn\">☰</button>
+  </div>
 </header>")
 
 
@@ -61,19 +79,19 @@
 
 (defui ChatMessageSent
   "<li class=\"chat-msg owner\">
-  <img src=\"http://i.imgur.com/1rm5Qq6.png\" class=\"avatar\">
+  <img src=\"https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/3/000/024/157/2fd1a24.jpg\" class=\"avatar\">
   <div class=\"bubble\">
-    <div class=\"bubble-uname_owner\">Alexey Veligura</div>
-    <div class=\"body\">Так как многие не попадают в том числе я ). С вас хорошая трансляция или запись ))</div>
+    <div class=\"bubble-uname_owner\">Rich Hickey</div>
+    <div class=\"body\">Cool!</div>
   </div>
 </li>")
 
 (defui ChatMessageReceived
   "<li class=\"chat-msg\">
-  <img src=\"http://i.imgur.com/1rm5Qq6.png\" class=\"avatar\">
+  <img src=\"https://2013.webrebels.org/gfx/speaker_david.jpg\" class=\"avatar\">
   <div class=\"bubble\">
-    <div class=\"bubble-uname\">Alexey Veligura</div>
-    <div class=\"body\">Так как многие не попадают в том числе я ). С вас хорошая трансляция или запись ))</div>
+    <div class=\"bubble-uname\">David Nolen</div>
+    <div class=\"body\">The amount of innovation and invention in the ClojureScript community is nothing short of stunning.</div>
   </div>
 </li>")
 
@@ -82,8 +100,8 @@
 
 (defui ChatMessageBubble
   "<div class=\"bubble\">
-  <div class=\"bubble-uname_owner\">Alexey Veligura</div>
-  <div class=\"body\">Так как многие не попадают в том числе я ). С вас хорошая трансляция или запись ))</div>
+  <div class=\"bubble-uname_owner\">Rich Hickey</div>
+  <div class=\"body\">Cool!</div>
 </div>")
 
 
@@ -91,5 +109,6 @@
   "<footer class=\"app-footer\">
   <form class=\"message\">
     <input type=\"text\" class=\"message\">
+    <button class=\"btn send-btn\">Send</button>
   </form>
 </footer>")
